a1c28e84ed786499d87389182c497ee80e74d6c1
Mari <43574832+marisoto@users.noreply.github.com>
2025-05-01 21:18:45 -0400

    Update page.tsx

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -23,7 +23,7 @@ export default async function Page(props: {
         <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
       </div>
       <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
-        
+        <Search placeholder="Search invoices..." />
         <CreateInvoice />
       </div>
       <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
@@ -33,5 +33,5 @@ export default async function Page(props: {
         {/* <Pagination totalPages={totalPages} /> */}
       </div>
     </div>
-  )
+  );
 }

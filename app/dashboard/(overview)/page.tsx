bc2bba3d49a1c09771361bbf7045a88f497c0014
Mari <43574832+marisoto@users.noreply.github.com>
2025-05-01 21:01:36 -0400

    Chapter 11 - Search

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,10 +2,14 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
+import { fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
+import { Suspense } from 'react';
+import { RevenueChartSkeleton,
+  LatestInvoicesSkeleton,
+  CardsSkeleton, } from '@/app/ui/skeletons';
+import CardWrapper from '@/app/ui/dashboard/cards';
  
 export default async function Page() {
-  const revenue = await fetchRevenue();
   const latestInvoices = await fetchLatestInvoices();
   const {
     numberOfInvoices,
@@ -19,9 +23,10 @@ export default async function Page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        {<Card title="Collected" value={totalPaidInvoices} type="collected" /> }
-        { <Card title="Pending" value={totalPendingInvoices} type="pending" /> }
-        { <Card title="Total Invoices" value={numberOfInvoices} type="invoices" /> }
+      <Suspense fallback={<CardsSkeleton />}>
+          <CardWrapper />
+        </Suspense>
+        
         {<Card
           title="Total Customers"
           value={numberOfCustomers}
@@ -29,7 +34,9 @@ export default async function Page() {
         /> }
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        {<RevenueChart revenue={revenue}  /> }
+      <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
         {<LatestInvoices latestInvoices={latestInvoices} /> }
       </div>
     </main>
